772235027b6cee3d4cbb50faeedc986a63b7b56d
Samuykti <administrator@samyukti.com>
2022-07-11 01:17:02 +0400

    Items

--- index.ts
+++ index.ts
@@ -6,12 +6,14 @@ import { dump } from 'js-yaml'
 const head = Spot.parseContract("./src/api.ts")
 const codes = Spot.parseContract("./src/codes.ts")
 const categories = Spot.parseContract("./src/categories.ts")
+const items = Spot.parseContract("./src/items.ts")
 
 const api = merge(
   [
     { oas: JSON.parse(JSON.stringify(Spot.OpenApi3.generateOpenAPI3(head))) },
     { oas: JSON.parse(JSON.stringify(Spot.OpenApi3.generateOpenAPI3(codes))) },
-    { oas: JSON.parse(JSON.stringify(Spot.OpenApi3.generateOpenAPI3(categories)))}
+    { oas: JSON.parse(JSON.stringify(Spot.OpenApi3.generateOpenAPI3(categories)))},
+    { oas: JSON.parse(JSON.stringify(Spot.OpenApi3.generateOpenAPI3(items)))},
   ]
 )
 
